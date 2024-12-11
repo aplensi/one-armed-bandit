73de6e4699b90028ebdba4ca7e56d480b08b6e48
aplensi <romashev.n16@gmail.com>
2024-12-11 22:13:21 +0300

    Added an interface

---
+++ one-armed bandit.cpp
@@ -0,0 +1,37 @@
+﻿#include "one-armed bandit.h"
+
+using namespace std;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    sf::RenderWindow window(sf::VideoMode(widthWindow, heightWindow), "one-armed bandit", sf::Style::Close);
+
+    if (!interfaceTexture.loadFromFile("images/menuOfOne-armedBandit.png")){
+        return -1;
+    }
+
+    interfaceSprite.setTexture(interfaceTexture);
+
+    sf::Vector2u windowSize = window.getSize();
+
+    interfaceSprite.setScale(
+        static_cast<float>(windowSize.x) / interfaceTexture.getSize().x,
+        static_cast<float>(windowSize.y) / interfaceTexture.getSize().y
+    );
+
+    // Основной цикл
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(interfaceSprite);
+        window.display();
+    }
+
+    return 0;
+}
---
+++ one-armed bandit.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+const int widthWindow = 960;
+const int heightWindow = 540;
+
+sf::Texture interfaceTexture;
+sf::Sprite interfaceSprite;
