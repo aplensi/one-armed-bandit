9b59e34bf10a3b94d4055bf4c88d1498363ac28c
aplensi <romashev.n16@gmail.com>
2024-12-13 00:45:06 +0300

    Added column of items and items

---
+++ roulette.cpp
@@ -0,0 +1,60 @@
+#include "roulette.h"
+
+roulette::roulette(std::string path, int numb, int startPosX, int startPosY) : 
+	numb(numb), startPosX(startPosX), startPosY(startPosY)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	for (int i = 1; i <= numb; i++)
+	{
+		items.emplace_back("images/itemOfRoulette", 7, i);
+		
+	}
+	std::shuffle(items.begin(), items.end(), gen);
+
+	
+}
+
+void roulette::render(sf::RenderWindow& window)
+{
+	for (int i = 0; i < items.size(); i++)
+	{
+		items[i].render(window);
+		items[i].position(startPosX, (i * startPosY) - 60);
+	}
+}
+
+void roulette::move(float deltaTime, float speed)
+{
+
+}
+
+itemOfRoulette::itemOfRoulette(std::string path, float size, int numb) :
+	path(path), size(size), numb(numb)
+{
+	texture->loadFromFile(path + std::to_string(numb) + std::string(".png"));
+	sprite->setTexture(*texture);
+	sprite->setScale(size, size);
+}
+
+void itemOfRoulette::render(sf::RenderWindow& window)
+{
+	window.draw(*sprite);
+}
+
+void itemOfRoulette::position(float x, float y)
+{
+	this->x = x;
+	this->y = y;
+	sprite->setPosition(x, y);
+}
+
+void itemOfRoulette::move(float deltaTime, float speed)
+{
+	sprite->move(0, speed * deltaTime);
+	if (sprite->getPosition().y > 540)
+	{
+		sprite->setPosition(x, y);
+	}
+}
---
+++ roulette.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+#include <random>
+
+class itemOfRoulette {
+public:
+	itemOfRoulette(std::string path, float size, int numb);
+	void render(sf::RenderWindow& window);
+	void position(float x, float y);
+	void move(float deltaTime, float speed);
+private:
+	std::string path;
+	float size, x, y;
+	int numb;
+	sf::Texture* texture = new sf::Texture;
+	sf::Sprite* sprite = new sf::Sprite;
+};
+
+class roulette {
+public:
+	roulette(std::string path, int numb, int startPosX, int startPosY);
+	void render(sf::RenderWindow& window);
+	void move(float deltaTime, float speed);
+private:
+	int numb, startPosX, startPosY;
+	float speedOfScroll;
+	std::vector<itemOfRoulette> items;
+	std::string path;
+};
